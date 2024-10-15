9cc30dc91f52475306079bc8cd4acef3b76ec354
Clément Sauvage <cl.sauvage@yahoo.com>
2024-10-15 08:53:20 +0200

    Add tview dependency

--- cmd/kafe/main.go
+++ cmd/kafe/main.go
@@ -1,9 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "github.com/rivo/tview"
 
 func main() {
-	fmt.Println("Hello, World!")
+	app := tview.NewApplication()
+	topicList := tview.NewList().
+		AddItem("Topic 1", "Description 1", 'a', nil).
+		AddItem("Topic 2", "Description 2", 'b', nil).
+		AddItem("Topic 3", "Description 3", 'c', nil).
+		AddItem("Quit", "Press to exit", 'q', func() {
+			app.Stop()
+		})
+
+	topicList.SetBorder(true).SetTitle("Kafka Topics")
+
+	if err := app.SetRoot(topicList, true).Run(); err != nil {
+		panic(err)
+	}
 }
