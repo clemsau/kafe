f842c3d68635f2036b6fc41a072fa3ed1698e149
Clément Sauvage <cl.sauvage@yahoo.com>
2024-10-19 13:14:13 +0200

    Add sarama library & Read local cluster with it

--- cmd/kafe/main.go
+++ cmd/kafe/main.go
@@ -1,20 +1,93 @@
 package main
 
-import "github.com/rivo/tview"
+import (
+	"log"
+	"sort"
+	"time"
+
+	"github.com/IBM/sarama"
+	"github.com/rivo/tview"
+)
 
 func main() {
+	config := sarama.NewConfig()
+	config.Version = sarama.V2_8_0_0
+
+	client, err := sarama.NewClient([]string{"localhost:9092"}, config)
+	if err != nil {
+		log.Fatalf("Error creating client: %v", err)
+	}
+	defer client.Close()
+
 	app := tview.NewApplication()
 	topicList := tview.NewList().
-		AddItem("Topic 1", "Description 1", 'a', nil).
-		AddItem("Topic 2", "Description 2", 'b', nil).
-		AddItem("Topic 3", "Description 3", 'c', nil).
-		AddItem("Quit", "Press to exit", 'q', func() {
-			app.Stop()
-		})
+		SetChangedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {})
+
+	topicList.AddItem("Fetching topics...", "", 0, nil)
+	topicList.AddItem("Quit", "Press to exit", 'q', func() {
+		app.Stop()
+	})
 
 	topicList.SetBorder(true).SetTitle("Kafka Topics")
 
-	if err := app.SetRoot(topicList, true).Run(); err != nil {
-		panic(err)
+	// Channel to send topic updates
+	updateChan := make(chan []string)
+
+	// Start Kafka monitoring in a separate goroutine
+	go monitorTopics(client, updateChan)
+
+	// Handle updates in the main thread
+	go func() {
+		for topics := range updateChan {
+			func(t []string) {
+				app.QueueUpdateDraw(func() {
+					topicList.Clear()
+					for _, topic := range t {
+						topicList.AddItem(topic, "", 0, nil)
+					}
+					topicList.AddItem("Quit", "Press to exit", 'q', func() {
+						app.Stop()
+					})
+				})
+			}(topics)
+		}
+	}()
+
+	if err := app.SetRoot(topicList, true).SetFocus(topicList).Run(); err != nil {
+		log.Fatalf("Error running application: %v", err)
 	}
+}
+
+func monitorTopics(client sarama.Client, updateChan chan<- []string) {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	var currentTopics []string
+
+	for ; ; <-ticker.C {
+		topics, err := client.Topics()
+		if err != nil {
+			log.Printf("Error fetching topics: %v", err)
+			continue
+		}
+
+		sort.Strings(topics)
+
+		if !equal(topics, currentTopics) {
+			currentTopics = topics
+			updateChan <- topics
+		}
+	}
+}
+
+func equal(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
 }
--- go.mod
+++ go.mod
@@ -3,13 +3,35 @@ module github.com/clemsau/kafe
 go 1.22.0
 
 require (
-	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/gdamore/tcell/v2 v2.7.1 // indirect
+	github.com/IBM/sarama v1.43.3
+	github.com/rivo/tview v0.0.0-20241016194538-c5e4fb24af13
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.7.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/gdamore/encoding v1.0.1 // indirect
+	github.com/gdamore/tcell/v2 v2.7.4 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/tview v0.0.0-20240921122403-a64fc48d7654 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/sys v0.17.0 // indirect
-	golang.org/x/term v0.17.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/term v0.25.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
 )
